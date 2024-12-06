eb9c22ae79acfed3651aa210d3e8fa16a89e54b0
Alex Schneider <alex@schneids.me>
2024-12-05 22:14:17 -0800

    Less brute force

--- src/main/java/aoc/day06/Day06.java
+++ src/main/java/aoc/day06/Day06.java
@@ -4,43 +4,35 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import com.google.common.collect.Sets;
 import aoc.Day;
 import aoc.Utils;
 
 public class Day06 implements Day {
 
+    Set<List<Integer>> grid;
+    int startingGuardX;
+    int startingGuardY;
+    int xSize;
+    int ySize;
+
     @Override
     public String part1(String input) {
-        var lines = Utils.splitLines(input);
-        var grid = Sets.newHashSet();
-        var guardX = 0;
-        var guardY = 0;
-        for (var x = 0; x < lines.size(); x++) {
-            for (var y = 0; y < lines.get(x).length(); y++) {
-                var ch = lines.get(x).charAt(y);
-                if (ch == '#') {
-                    grid.add(Pair.of(x, y));
-                } else if (ch == '^') {
-                    guardX = x;
-                    guardY = y;
-                }
-            }
-        }
+        init(input);
         var xDir = -1;
         var yDir = 0;
         var visited = Sets.newHashSet();
-        while (guardX >= 0 && guardY >= 0 && guardX < lines.size() && guardY < lines.get(guardX).length()) {
-            if (grid.contains(Pair.of(guardX, guardY))) {
+        var guardX = this.startingGuardX;
+        var guardY = this.startingGuardY;
+        while (isWithinGrid(guardX, guardY)) {
+            if (this.grid.contains(List.of(guardX, guardY))) {
                 guardX -= xDir;
                 guardY -= yDir;
                 var swap = yDir;
                 yDir = -xDir;
                 xDir = swap;
             } else {
-                visited.add(Pair.of(guardX, guardY));
+                visited.add(List.of(guardX, guardY));
                 guardX += xDir;
                 guardY += yDir;
             }
@@ -48,14 +40,65 @@ public class Day06 implements Day {
         return Integer.toString(visited.size());
     }
 
-    private boolean isLoop(Set<Pair<Integer, Integer>> grid, int guardX, int guardY, int xSize, int ySize) {
+    @Override
+    public String part2(String input) {
+        init(input);
+
+        var xDir = -1;
+        var yDir = 0;
+        var guardX = this.startingGuardX;
+        var guardY = this.startingGuardY;
+        var seenLoops = Sets.newHashSet();
+        while (isWithinGrid(guardX, guardY)) {
+            if (this.grid.contains(List.of(guardX, guardY))) {
+                guardX -= xDir;
+                guardY -= yDir;
+                var swap = yDir;
+                yDir = -xDir;
+                xDir = swap;
+            } else {
+                guardX += xDir;
+                guardY += yDir;
+                if (!seenLoops.contains(List.of(guardX, guardY)) && isLoopWithAdded(guardX, guardY)) {
+                    seenLoops.add(List.of(guardX, guardY));
+                }
+            }
+        }
+        return Integer.toString(seenLoops.size());
+    }
+
+    private void init(String input) {
+        var lines = Utils.splitLines(input);
+        this.xSize = lines.size();
+        this.ySize = lines.get(0).length();
+        this.grid = Sets.newHashSet();
+        for (var x = 0; x < lines.size(); x++) {
+            for (var y = 0; y < lines.get(x).length(); y++) {
+                var ch = lines.get(x).charAt(y);
+                if (ch == '#') {
+                    this.grid.add(List.of(x, y));
+                } else if (ch == '^') {
+                    this.startingGuardX = x;
+                    this.startingGuardY = y;
+                }
+            }
+        }
+    }
+
+    private boolean isWithinGrid(int guardX, int guardY) {
+        return guardX >= 0 && guardY >= 0 && guardX < this.xSize && guardY < this.ySize;
+    }
+
+    private boolean isLoopWithAdded(int addX, int addY) {
         var xDir = -1;
         var yDir = 0;
         var visited = Sets.newHashSet();
-        while (guardX >= 0 && guardY >= 0 && guardX < xSize && guardY < ySize) {
+        var guardX = this.startingGuardX;
+        var guardY = this.startingGuardY;
+        while (isWithinGrid(guardX, guardY)) {
             if (visited.contains(List.of(guardX, guardY, xDir, yDir))) {
                 return true;
-            } else if (grid.contains(Pair.of(guardX, guardY))) {
+            } else if ((guardX == addX && guardY == addY) || this.grid.contains(List.of(guardX, guardY))) {
                 guardX -= xDir;
                 guardY -= yDir;
                 visited.remove(List.of(guardX, guardY, xDir, yDir));
@@ -71,36 +114,4 @@ public class Day06 implements Day {
         return false;
     }
 
-    @Override
-    public String part2(String input) {
-        var lines = Utils.splitLines(input);
-        var grid = new HashSet<Pair<Integer, Integer>>();
-        var guardX = 0;
-        var guardY = 0;
-        var xSize = lines.size();
-        var ySize = lines.get(0).length();
-        for (var x = 0; x < xSize; x++) {
-            for (var y = 0; y < ySize; y++) {
-                var ch = lines.get(x).charAt(y);
-                if (ch == '#') {
-                    grid.add(Pair.of(x, y));
-                } else if (ch == '^') {
-                    guardX = x;
-                    guardY = y;
-                }
-            }
-        }
-        var sum = 0;
-        for (int x = 0; x < xSize; x++) {
-            for (int y = 0; y < ySize; y++) {
-                var thisSet = new HashSet<Pair<Integer, Integer>>();
-                thisSet.addAll(grid);
-                thisSet.add(Pair.of(x, y));
-                if (isLoop(thisSet, guardX, guardY, xSize, ySize)) {
-                    sum += 1;
-                }
-            }
-        }
-        return Integer.toString(sum);
-    }
 }
